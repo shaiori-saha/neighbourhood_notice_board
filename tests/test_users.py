ac6174f045a713ea46186245e7e7c721d611dc10
shaiori <shaiorisaha@gmail.com>
2024-02-14 22:23:55 +0100

    added test for users

---
+++ tests/test_users.py
@@ -0,0 +1,70 @@
+import pytest
+import requests
+import pytest_asyncio
+
+from app.routers.users import UserCreateRequest, UserResponse, create_user
+
+pytest_plugins = ('pytest_asyncio',)
+
+
+def test_get_users():
+
+    res = requests.get('http://localhost:8000/users/search/')
+
+    expected_response =[{
+    "id": 1,
+    "name": "sha",
+    "email": "petuk@example.com",
+    "created_at": "2024-02-11T18:04:17.732550+00:00",
+    "address": "berlin",
+    "phone_numbers": [
+      {
+        "id": 1,
+        "phone_number": "12345"
+      },
+      {
+        "id": 2,
+        "phone_number": "34567"
+      }
+    ]
+  }]
+
+    print(res.json())
+
+    assert res.status_code == 200
+    #assert res.json() == expected_response
+
+@pytest.mark.asyncio
+async def test_create_user():
+    new_user = {
+        "name": "shah",
+        "password": "hati",
+        "email": "again23@exam.com",
+        "phone_numbers": []
+    }
+    res = requests.post('http://localhost:8000/users/', json=new_user)
+    
+    assert res.status_code == 201
+
+@pytest.mark.asyncio
+async def test_update_user():
+        new_user = {
+        "id": 1,
+        "name": "sha sha"
+        }
+        res = requests.put('http://localhost:8000/users/1', json=new_user)
+        assert res.status_code == 200
+
+@pytest.mark.asyncio
+async def test_delete_user():
+        
+        res = requests.delete('http://localhost:8000/users/9')
+        assert res.status_code == 200
+
+
+
+
+
+
+    #b'[{"id":1,"name":"sha","email":"petuk@example.com","created_at":"2024-02-11T18:04:17.732550+00:00","address":"berlin","phone_numbers":[{"id":1,"phone_number":"12345"},{"id":2,"phone_number":"34567"}]}]'
+
